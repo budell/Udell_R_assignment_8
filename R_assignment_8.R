ab83645fe03eb760c47345bd8dd577e0cad9679a
Brad Udell <bradjudell@ufl.edu>
2015-11-13 12:26:31 -0500

    I updated the import data function so that the recenlty extinct observation were removed from the analysis.

--- R_assignment_8.R
+++ R_assignment_8.R
@@ -3,16 +3,19 @@
 get_data <- function(){
   #first we use read.csv to querry the downloaded flie.  -999 is treated as NAs, and strings as factors.
   data <- read.csv("data/MOMv3.3.txt",sep = "\t",header=F,stringsAsFactors = FALSE, na.strings = "-999")
+  
   # Here the function adds collumn names to the data file
   colnames(data) <- c("continent", "status", "order", 
                               "family", "genus", "species", "log_mass", "combined_mass", 
                               "reference")
+  #this line removes the mammals that went extinct in the recent past
+  data <- subset(data,status != "historical")
+  
   return(data)
 }
 
 
-
-
+#here is a function to 
 
 
 
